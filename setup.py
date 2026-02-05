e758b4ad64a54e3d8f18f72036181a5fb0fc6d50
Ben Latter <ben.latter@hiddenlevel.com>
2026-02-05 13:23:33 -0500

    Fix memory leak from unreleased buffers in pack_into

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="cbitstruct",
-    version="1.1.1",
+    version="1.1.2",
     author="Quentin CHATEAU",
     author_email="quentin.chateau@gmail.com",
     license="MPL-2.0",
